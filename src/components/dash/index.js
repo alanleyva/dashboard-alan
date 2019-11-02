f6340008a5927935a27eaf80be8b941b6ffaae5e
Alan Leyva <alan@vamedia.co>
2019-11-02 13:20:52 -0700

    Added dashboard elements

--- src/components/dash/index.js
+++ src/components/dash/index.js
@@ -3,11 +3,12 @@ import { List } from "../users/lists";
 import { Tasks } from "./tasks";
 import { PageTitle } from "../page/page_title";
 import { Projects } from "./projects";
+import { Users as Contacts } from "../users/user_cards";
 
 const Dashboard = () => (
   <div className="page-inner">
     <PageTitle title="Dashboard" description="Where do you want to go" />
-    <div class="card-deck-xl">
+    <div className="card-deck-xl">
       <div className="card card-fluid pb-0">
         <div className="card-header">Users</div>
         <List />
@@ -17,9 +18,16 @@ const Dashboard = () => (
         <Tasks />
       </div>
     </div>
-    <div class="card-deck-xl">
-      <div class="card card-fluid pb-3">
-        <div class="card-header"> Active Projects </div>
+    <div className="card-deck-xl">
+      <div className="card card-fluid pb-3">
+        <div className="card-header"> Last Contacts </div>
+        <div className="card-body">
+          <Contacts />
+        </div>
+      </div>
+
+      <div className="card card-fluid pb-3">
+        <div className="card-header"> Active Projects </div>
         <Projects />
       </div>
     </div>
---
+++ src/components/users/user_cards.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+export const Users = () => (
+  <React.Fragment>
+    <UserCard
+      image="https://randomuser.me/api/portraits/women/79.jpg"
+      name="Gabriela Carvalho"
+      title="Pharmacist"
+    />
+    <UserCard
+      image="https://pbs.twimg.com/profile_images/1099856449918943232/hKv3Fddh.jpg"
+      name="Diogo Lima"
+      title="Insurance Agent"
+    />
+    <UserCard
+      image="https://images.unsplash.com/photo-1476493279419-b785d41e38d8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=61eaea85f1aa3d065400179c78163f15"
+      name="Isabelle Pereira"
+      title="Psychologist"
+    />
+  </React.Fragment>
+);
+
+var UserCard = props => (
+  <div className="media align-items-center mb-3">
+    <div className="col-auto">
+      <a href="user-profile.html" className="user-avatar user-avatar-xl">
+        <img src={props.image} alt="" />
+      </a>
+    </div>
+
+    <div className="col">
+      <h3 className="card-title text-truncate">
+        <a href="user-profile.html">{props.name}</a>
+      </h3>
+      <h6 className="card-subtitle text-muted"> {props.title} </h6>
+    </div>
+
+    <div className="col-auto">
+      <button
+        type="button"
+        className="btn btn-icon btn-secondary mr-1"
+        data-toggle="tooltip"
+        title=""
+        data-original-title="Private message"
+      >
+        <i className="far fa-comment-alt"></i>
+      </button>
+      <div className="dropdown d-inline-block">
+        <button className="btn btn-icon btn-secondary" data-toggle="dropdown">
+          <i className="fa fa-fw fa-ellipsis-h"></i>
+        </button>
+        <div className="dropdown-menu dropdown-menu-right">
+          <div className="dropdown-arrow"></div>
+          <button type="button" className="dropdown-item">
+            Invite to a team
+          </button>{" "}
+          <button type="button" className="dropdown-item">
+            Copy member ID
+          </button>
+          <div className="dropdown-divider"></div>
+          <button type="button" className="dropdown-item">
+            Remove
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
